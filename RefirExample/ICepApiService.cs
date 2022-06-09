7d8c0379a50e5c3fb0f4b5738935c68fd9d81c88
Sanderson Silva Corrêa <correasanderson@gmail.com>
2022-06-09 16:49:24 -0300

    Add project files.

---
+++ RefirExample/ICepApiService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace RefirExample
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<ViaCepResponse> GetAddressAsync(string cep);
+    }
+}
---
+++ RefirExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Refit;
+
+namespace RefirExample
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.WriteLine("Informe seu CEP:");
+
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultando informações...");
+
+                var address = await cepClient.GetAddressAsync(cepInformado);
+
+                Console.WriteLine($"CEP: {address.cep}\nLogradouro: {address.logradouro}\nComplemento: {address.complemento}\nBairro: {address.bairro}\n");
+                Console.WriteLine($"Localidade: {address.localidade}\nUF: {address.uf}\nIBGE: {address.ibge}\nGIA: {address.gia}\n");
+                Console.WriteLine($"DDD: {address.ddd}\nSIAFI: {address.siafi}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
